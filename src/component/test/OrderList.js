da65a117abf0b9968abf48f43ba039f8497ba760
Shih-Hao-Lo <slo@stevens.edu>
2020-03-27 22:48:09 -0400

    add geo data

---
+++ src/component/test/OrderList.js
@@ -0,0 +1,64 @@
+import React, { Component } from 'react';
+import $ from 'jquery';
+
+import Order from './order'
+
+class OrderList extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            info: "fetching...",
+            data: [],
+            lat: 0,
+            long: 0
+        }
+    }
+
+    componentDidMount() {
+        if ("geolocation" in navigator) {
+            // check if geolocation is supported/enabled on current browser
+            navigator.geolocation.getCurrentPosition(
+                function success(position) {
+                    // for when getting location is a success
+                    console.log('latitude', position.coords.latitude,'longitude', position.coords.longitude);
+                    this.setState({
+                        lat: position.coords.latitude,
+                        long: position.coords.longitude
+                    });
+                }.bind(this),
+                function error(error_message) {
+                    // for when getting location results in an error
+                    console.error('An error has occured while retrievinglocation', error_message)
+                }
+            );
+        } else {
+            // geolocation is not supported
+            // get your location some other way
+            console.log('geolocation is not enabled on this browser')
+        }
+        $.ajax({
+            url: 'http://localhost:3001/get_all_orders',
+            type: 'GET',
+            success: function (res) {
+                alert('complete!')
+                console.log(res);
+                this.setState({
+                    info: "completed",
+                    data: res
+                })
+            }.bind(this)
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Current Loaction:</h1>
+                <h1>[{this.state.lat} , {this.state.long}]</h1>
+                {this.state.data.map(order => <Order key={order._id} order={order} />)}
+            </div>
+        )
+    }
+}
+
+export default OrderList;
---
+++ src/component/test/order.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+
+class Order extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            data: this.props.order
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <h2>Post by: {this.state.data.user.username}</h2>
+                <h4>Poster Location: [{this.state.data.user.Lat},{this.state.data.user.Long_}]</h4>
+                <h4>Offer Product: {this.state.data.prod}</h4>
+                <h4>Quantity: {this.state.data.amt}</h4>
+                <h4>Desired Product: {this.state.data.wish}</h4>
+                <h4>Quantity: {this.state.data.wish_amt}</h4>
+
+                <h4>Contact Info:</h4>
+                <h4>{this.state.data.user.email}</h4>
+
+                <h5>Opining Status: {this.state.data.status}</h5>
+                <h5>Reserved By: {this.state.data.reserved_by}</h5>
+            </div>
+        )
+    }
+}
+
+
+export default Order;
